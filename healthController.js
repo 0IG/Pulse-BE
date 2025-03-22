b0d9ee46e927e825eeed3a06d1a3d93c91917588
Isaac Gonzalez <u1337x@gmail.com>
2025-03-22 17:29:54 -0400

    Branch setups

--- healthController.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const healthController = express()
-
-healthController.get("/", (req, res) => {
-    res.send("healthy")
-})
-
-module.exports = healthController
